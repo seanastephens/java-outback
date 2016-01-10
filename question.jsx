44d6b0499c36fa18f636b7100ceb70636962dc9a
Sean Stephens <seanastephens@email.arizona.edu>
2016-01-09 20:11:52 -0800

    Merge

--- question.jsx
+++ question.jsx
@@ -2,77 +2,80 @@
 
 import React from 'react';
 
+const NOT_ANSWERED = { state: 'not-answered' };
+const CORRECT_ANSWER = { state: 'correct' };
+const INCORRECT_ANSWER = { state: 'incorrect' };
+
 export default React.createClass( {
 	getInitialState() {
-		return { explanation: false, congratulations: false };
+		return NOT_ANSWERED;
 	},
 	render() {
-		const revealIncorrect = () => {
-			this.setState(Object.assign(this.state, { explanation: true }));
-		};
-		const revealCorrect = () => {
-			this.setState(Object.assign(this.state, { congratulations: true }));
-		};
-
-		const parentOnCorrect = this.props.onCorrect;	
-		const parentOnIncorrect = this.props.onIncorrect;	
+		const revealIncorrect = () => this.setState(INCORRECT_ANSWER);
+		const revealCorrect = () => this.setState(CORRECT_ANSWER);
 
 		const nextQuestionCorrect = () => {
-			this.setState(this.getInitialState());
-			parentOnCorrect();
+			this.setState(NOT_ANSWERED);
+			this.props.onCorrect();
 		};
 		const nextQuestionIncorrect = () => {
-			this.setState(this.getInitialState());
-			parentOnIncorrect();
+			this.setState(NOT_ANSWERED);
+			this.props.onIncorrect();
 		};
 
 		const question = this.props.question;
 
-		const answers = question.bogus
-			.map(content => ({ correct: false, content, callback: revealIncorrect }))
-			.concat([ { correct: true, content: question.answer, callback: revealCorrect } ]);
+		const answer = (content, correct) => ({ content, correct });
+
+		const badAnswers = question.bogus.map(content => answer(content, false));
+		const allAnswers = Array.prototype.concat.apply(badAnswers, answer(question.answer, true));
 
 		const styleForButton = {
 			true: { backgroundColor: '#6f6' },
 			false: { backgroundColor: '#f66' }
 		};
-		const answered = this.state.explanation || this.state.congratulations;
-		const explanation = this.state.explanation;
 
 		return (<div>
 				<h4>{ question.line }</h4>
 				{ 
-					answers.map((answer, i) => {
+					allAnswers.map((answer, i) => {
 						const buttonColor = styleForButton[answer.correct];
 						return (
 							<div key={i}>
 								<button 
-								className={'btn ' + (answered ? '' : 'btn-default' )}
+								className='btn btn-default'
 								style={answered ? buttonColor : null}
-								onClick={ answer.callback }>{ answer.content }</button>
+								onClick={ answer.correct ? revealCorrect : revealIncorrect }>
+								{ answer.content }
+								</button>
 							</div>
 						);
 					})
 				}
-				<div>
-					<h4 style={{color: (explanation ? 'red' : 'black')}} >{ 
-						(() => {
-							if(this.state.explanation)
-								return question.explanation;
-							if(this.state.congratulations)
-								return "Correct";
-							return null
-						})()
-					}</h4>
-
-					<button 
-					className='btn btn-default'
-					onClick={ 
-						this.state.explanation ? nextQuestionIncorrect : nextQuestionCorrect 
-					}
-					type="button"
-					disabled={!answered}>Next</button>
-				</div>
+				{
+					(() => {
+						const text = {
+							[NOT_ANSWERED.state]: '',
+							[CORRECT_ANSWER.state]: 'Correct',
+							[INCORRECT_ANSWER.state]: question.explanation,
+						}[this.state.state];
+						const color = (this.state !== NOT_ANSWERED) ? 'red' : 'black';
+						const callback = 
+							(this.state === INCORRECT_ANSWER ? nextQuestionIncorrect : nextQuestionCorrect);
+						return (
+							<div>
+								<h4 style={{color}}>{text}</h4>
+								<button 
+								className='btn btn-default'
+								onClick={callback}
+								type="button"
+								disabled={this.state === NOT_ANSWERED}>
+								Next
+								</button>
+							</div>
+						);
+					})()
+				}
 			</div>);
 	}
 });
