d09f387ad49d2633c176927766519f1b71575170
Sean Stephens <seanastephens@email.arizona.edu>
2017-05-05 21:13:45 -0700

    Fix formatting in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 var webpack = require( 'webpack' );
 
 module.exports = {
-	entry: './src/index.jsx',
-	// output: {
-	// 	App: [
-	// 		'webpack-dev-server/client?http://localhost:4001',
-	// 		'webpack/hot/only-dev-server',
-	// 		'./index.jsx'
-	// 	]
-	// },
-	output: {
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				loaders: [ 'babel-loader' ]
+  entry: './src/index.jsx',
+  // output: {
+  //   App: [
+  //     'webpack-dev-server/client?http://localhost:4001',
+  //     'webpack/hot/only-dev-server',
+  //     './index.jsx'
+  //   ]
+  // },
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: [ 'babel-loader' ]
       }
-		]
-	},
-	plugins: [
-		new webpack.NoErrorsPlugin()
-	].concat(process.env.NODE_ENV === 'production' ? [
+    ]
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ].concat(process.env.NODE_ENV === 'production' ? [
     new webpack.DefinePlugin({
       'process.env': {
           NODE_ENV: JSON.stringify('production')
@@ -31,9 +31,9 @@ module.exports = {
     }),
     new webpack.optimize.UglifyJsPlugin()
   ] : []),
-	resolve: {
-		modulesDirectories: [ '', 'lib', 'node_modules' ],
-		extensions: [ '', '.js', '.jsx' ]
-	},
+  resolve: {
+    modulesDirectories: [ '', 'lib', 'node_modules' ],
+    extensions: [ '', '.js', '.jsx' ]
+  },
   devtool: 'sourcemap'
 };
